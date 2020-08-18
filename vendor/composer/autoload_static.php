1d147b9e83f165acec2144499bd7f37a1cd271a9
Dayanand <dayanand@kribhcoindia.com>
2020-08-18 16:51:43 +0530

    composer install

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -472,9 +472,9 @@ class ComposerStaticInitb61204997c11e4fe727082b49922ea84
         'Doctrine\\Common\\' => 
         array (
             0 => __DIR__ . '/..' . '/doctrine/common/lib/Doctrine/Common',
-            1 => __DIR__ . '/..' . '/doctrine/reflection/lib/Doctrine/Common',
-            2 => __DIR__ . '/..' . '/doctrine/event-manager/lib/Doctrine/Common',
-            3 => __DIR__ . '/..' . '/doctrine/persistence/lib/Doctrine/Common',
+            1 => __DIR__ . '/..' . '/doctrine/event-manager/lib/Doctrine/Common',
+            2 => __DIR__ . '/..' . '/doctrine/persistence/lib/Doctrine/Common',
+            3 => __DIR__ . '/..' . '/doctrine/reflection/lib/Doctrine/Common',
         ),
         'Composer\\Semver\\' => 
         array (
