4ba62368ffb5de8840239ae02f5bbd123ee7e25b
recommendsmart <recommendsmart1@gmail.com>
2022-08-31 12:56:03 +0530

    vendor composer change php

--- vendor/composer/platform_check.php
+++ vendor/composer/platform_check.php
@@ -4,9 +4,6 @@
 
 $issues = array();
 
-if (!(PHP_VERSION_ID >= 70400)) {
-    $issues[] = 'Your Composer dependencies require a PHP version ">= 7.3.0". You are running ' . PHP_VERSION . '.';
-}
 
 if ($issues) {
     if (!headers_sent()) {
